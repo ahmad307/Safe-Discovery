ee36a5e1ccef0ff2d2ee2d8e8e8ab10ff8a5672b
Ahmad <ahmad.hussain.307@gmail.com>
2018-10-20 12:25:51 +0200

    Added Safe Streets visualization

--- web_src/js/script.js
+++ web_src/js/script.js
@@ -1,19 +1,44 @@
+var data = [
+    [30.012815,31.289761,30.013502,31.287544, 'green'],
+    [30.013502,31.287544,30.014068,31.285439,'green'],
+    [30.014068,31.285439,30.015095,31.281898,'yellow'],
+    [30.013327,31.287314,30.012036,31.286758,'red'],
+    [30.012036,31.286758,30.010504,31.286185,'yellow'],
+    [30.012668,31.289687,30.010888,31.290153,'yellow'],
+    [30.011965,31.286937,30.011402,31.288732,'yellow'],
+    [30.011012,31.288624,30.011566,31.286756,'green'],
+    [30.011371,31.288813,30.011621,31.289887,'red'],
+    [30.010996,31.288651,30.009645,31.28912,'green'],
+    [30.010793,31.287207,30.010332,31.288831,'green'],
+    [30.011346,31.287348,30.010275,31.286953,'yellow'],
+    [30.012621,31.289707,30.010886,31.290185,'red'],
+    [30.013573,31.287538,30.01423,31.287773,'red'],
+    [30.014086,31.287568,30.014499,31.286058,'yellow']
+]
 /**
 * Adds a polyline between Dublin, London, Paris and Berlin to the map
 *
 * @param  {H.Map} map      A HERE Map instance within the application
 */
 function addPolylineToMap(map) {
-    var lineString = new H.geo.LineString();
+    for (var i=0;i<data.length;i++){
+        var lineString = new H.geo.LineString();
 
-    lineString.pushPoint({lat:30.0185290, lng:31.3261620});
-    lineString.pushPoint({lat:30.0915467955, lng:31.30040815});
-    //lineString.pushPoint({lat:48.8567, lng:2.3508});
-    //lineString.pushPoint({lat:52.5166, lng:13.3833});
+        lineString.pushPoint({lat:data[i][0], lng:data[i][1]});
+        lineString.pushPoint({lat:data[i][2], lng:data[i][3]});
 
-    map.addObject(new H.map.Polyline(
-    lineString, { style: { lineWidth: 4 }}
-    ));
+        var myColor;
+        if (data[i][4] == 'red')
+            myColor = '#ff0000';
+        else if (data[i][4] == 'yellow')
+            myColor = '#ffd900';
+        else if (data[i][4] == 'green')
+            myColor = '#0de99a';
+        
+        map.addObject(new H.map.Polyline(
+        lineString, { style: { lineWidth: 4, strokeColor: myColor }}
+        ));
+    }
 }
 
 
@@ -36,8 +61,8 @@ var defaultLayers = platform.createDefaultLayers({
 //Step 2: initialize a map - this map is centered over Europe
 var map = new H.Map(document.getElementById('map'),
 defaultLayers.normal.map,{
-    center: {lat:52, lng:5},
-    zoom: 5,
+    center: {lat:30.013, lng:31.28},
+    zoom: 15,
     pixelRatio: pixelRatio
 });
 
